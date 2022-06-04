27daeac537bf8db6f6ef2a33d8a92f86372e99d7
Md-Nuruddin-Adnan <nuruddin.adnan1993@gmail.com>
2022-06-04 11:20:49 +0600

    project complete

---
+++ assets/js/custom.js
@@ -0,0 +1,54 @@
+{
+  var swiper = new Swiper(".brandSwiper", {
+    freeMode: true,
+    autoplay: true,
+    slidesPerView: 6,
+    loop: true,
+    speed: 1500,
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+      slidesPerView: 3,
+      spaceBetween: 50,
+      },
+      // when window width is >= 992px
+      992: {
+      slidesPerView: 4,
+      spaceBetween: 50
+      },
+      // when window width is >= 1400px
+      1400: {
+      slidesPerView: 6,
+      spaceBetween: 60
+      }
+    }
+  });
+}
+
+{
+  var swiper = new Swiper(".testimonialSwiper", {
+    slidesPerView: "auto",
+    centeredSlides: true,
+    spaceBetween: 30,
+    autoplay: true,
+    speed: 1500,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 50,
+      },
+      // when window width is >= 480px
+      680: {
+        slidesPerView: "auto",
+        spaceBetween: 15,
+      }
+    }
+  });
+}
